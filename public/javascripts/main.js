c4f464e0df1f2f578e9ab11eb513fddb780be5ad
Oscar <oscargus0247@gmail.com>
2022-01-27 21:16:30 +0100

    Starting to test json fata

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -18,8 +18,34 @@ const load = (res) => {
 
     data.forEach(element => {
 
-        const li = document.createElement('li');
-        li.textContent = element.jobTitle;
-        document.getElementById('app').appendChild(li);
+        const col = document.createElement('div');
+        col.className = 'col';
+        document.getElementById('row').appendChild(col);
+
+        const card = document.createElement('div');
+        card.className = 'card m-3';
+        row.appendChild(card);
+
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        card.appendChild(cardBody);
+
+        const title = document.createElement('h2');
+        title.textContent = element.jobTitle;
+        title.className = '';
+
+        const company = document.createElement('h3');
+        company.textContent = element.companyName;
+
+        const location = document.createElement('h3');
+        location.textContent = element.companyLocation;
+
+        const published = document.createElement('h4');
+        published.textContent = element.jobPublished;
+
+        cardBody.appendChild(title);
+        cardBody.appendChild(company);
+        cardBody.appendChild(location);
+        cardBody.appendChild(published);
     });
 }
