f3b69e26f4b31eba034be0f58cc6b2f861777603
Oscar <oscargus0247@gmail.com>
2022-01-27 21:15:43 +0100

    Cleaning up route

--- routes/data.js
+++ routes/data.js
@@ -13,15 +13,4 @@ router.get('/', function(req, res, next) {
     }
 });
 
-router.get('/data', function(req, res, next) {
-    if (typeof req.headers.referer != 'undefined') {
-
-        const data = require('../data.json');
-        res.send(data);
-
-    } else {
-        res.send('<h1>Unathourized Access</h1>');
-    }
-});
-
 module.exports = router;
