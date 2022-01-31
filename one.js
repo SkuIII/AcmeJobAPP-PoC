56209656d1f770e5a0509a6dfcee9cf8c2a5bc6f
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-31 10:11:45 +0100

    scraping works in one file

--- one.js
+++ one.js
@@ -7,7 +7,7 @@ async function startBrowser() {
     try {
         console.log("Opening the browser......");
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: ["--disable-setuid-sandbox"],
             'ignoreHTTPSErrors': true
         });
@@ -30,10 +30,8 @@ scrapeAll(browserInstance)
 async function scrapeAll(browserInstance) {
     let browser;
     try {
-        console.log('BEFORE BROWSERINSTANCE');
         browser = await browserInstance;
         await scraperObject.scraper(browser);
-        console.log('AFTER BROWSERINSTANCE');
 
     } catch (err) {
         console.log("Could not resolve the browser instance => ", err);
@@ -43,53 +41,109 @@ async function scrapeAll(browserInstance) {
 
 
 const scraperObject = {
-    url: 'http://books.toscrape.com',
+    url: 'https://arbetsformedlingen.se/platsbanken/annonser?ot=6YE1_gAC_R2G&q=devops&l=2:CifL_Rzy_Mku',
     async scraper(browser) {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         // Navigate to the selected page
         await page.goto(this.url);
+        let scrapedData = [];
         // Wait for the required DOM to be rendered
-        await page.waitForSelector('.page_inner');
-        // Get the link to all the required books
-        let urls = await page.$$eval('section ol > li', links => {
-            // Make sure the book to be scraped is in stock
-            links = links.filter(link => link.querySelector('.instock.availability > i').textContent !== "In stock")
-                // Extract the links from the data
-            links = links.map(el => el.querySelector('h3 > a').href)
-            return links;
-        });
-
-        console.log(urls);
-
-        // Loop through each of those links, open a new page instance and get the relevant data from them
-        let pagePromise = (link) => new Promise(async(resolve, reject) => {
-            let dataObj = {};
-            let newPage = await browser.newPage();
-            await newPage.goto(link);
-            dataObj['bookTitle'] = await newPage.$eval('.product_main > h1', text => text.textContent);
-            dataObj['bookPrice'] = await newPage.$eval('.price_color', text => text.textContent);
-            dataObj['noAvailable'] = await newPage.$eval('.instock.availability', text => {
-                // Strip new line and tab spaces
-                text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "");
-                // Get the number of stock available
-                let regexp = /^.*\((.*)\).*$/i;
-                let stockAvailable = regexp.exec(text)[1].split(' ')[0];
-                return stockAvailable;
+        async function scrapeCurrentPage() {
+            await page.waitForSelector('.result-container');
+            // Get the link to all the required books
+            let urls = await page.$$eval('.header-container > h3 > a', links => {
+                return links.map(link => link.href);
             });
-            dataObj['imageUrl'] = await newPage.$eval('#product_gallery img', img => img.src);
-            dataObj['bookDescription'] = await newPage.$eval('#product_description', div => div.nextSibling.nextSibling.textContent);
-            dataObj['upc'] = await newPage.$eval('.table.table-striped > tbody > tr > td', table => table.textContent);
-            resolve(dataObj);
-            await newPage.close();
-        });
+            console.log(urls);
+            // Loop through each of those links, open a new page instance and get the relevant data from them
+            let pagePromise = (link) => new Promise(async(resolve, reject) => {
+                let dataObj = {};
+                let newPage = await browser.newPage();
+                await newPage.goto(link);
+                await newPage.waitForSelector('.jobb-container');
 
-        for (link in urls) {
-            let currentPageData = await pagePromise(urls[link]);
-            // scrapedData.push(currentPageData);
-            console.log(currentPageData);
+                // dataObj['jobTitle'] = await newPage.$eval('h1.spacing.break-title', text => text.textContent);
+                // dataObj['companyName'] = await newPage.$eval('#pb-company-name', text => text.textContent);
+                // dataObj['companyLocation'] = await newPage.$eval('#pb-job-location', text => text.textContent);
+                // dataObj['jobDescription'] = await newPage.$eval('.job-description', text => text.textContent);
+                // dataObj['jobTerms'] = await newPage.$eval('[translate="section-jobb-main-content.extent"]', text => text.nextElementSibling.textContent);
+                // dataObj['jobPublished'] = await newPage.$eval('[translate="section-jobb-about.published"]', text => text.textContent);
+
+                // const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                // const monthsSwedish = ['januari', 'februari', 'mars', 'april', 'maj', 'juni', 'juli', 'augusti', 'september', 'oktober', 'november', 'december'];
+
+                // const publishedDateSplit = dataObj['jobPublished'].split(' ');
+                // const publishedYearSplit = publishedDateSplit[3].split(',');
+                // const publishedTimeSplit = publishedDateSplit[5].split('.');
+
+                // monthsSwedish.forEach((element, counter) => {
+                //     if (publishedDateSplit[2] == element) {
+                //         publishedDateSplit[2] = months[counter];
+                //     }
+                // });
+
+                // const publishedDate = `${publishedDateSplit[1]} ${publishedDateSplit[2]} ${publishedYearSplit[0]} ${publishedTimeSplit[0]}:${publishedTimeSplit[1]}`;
+
+                // dataObj['jobPublishedDate'] = Date.parse(publishedDate);
+
+                dataObj['jobLink'] = link;
+
+                resolve(dataObj);
+                await newPage.close();
+            });
+
+            // for (let link = 0; link < urls.length; link++) {
+            //     let currentPageData = await pagePromise(urls[link]);
+            //     scrapedData.push(currentPageData);
+            //     console.log(currentPageData);
+            // }
+
+            // scrapedData.sort((element1, element2) => element2.jobPublishedDate - element1.jobPublishedDate)
+
+            // fs.writeFile("data.json", JSON.stringify(scrapedData), (err) => {
+            //     if (err)
+            //         console.log(err);
+            //     else {
+            //         console.log("File written successfully\n");
+            //     }
+            // });
+
+            // When all the data on this page is done, click the next button and start the scraping of the next page
+            // You are going to check if this button exist first, so you know if there really is a next page.
+            let nextButtonExist = true;
+            try {
+                const nextButtonExist = await page.$eval('digi-button.digi-navigation-pagination__button.digi-navigation-pagination__button--next.digi-navigation-pagination__button--hidden.sc-digi-navigation-pagination.sc-digi-button-h.sc-digi-button-s.hydrated');
+                nextButtonExist = false;
+
+            } catch (err) {
+                console.log(err);
+                nextButtonExist = true;
+            }
+            if (nextButtonExist) {
+
+                await page.waitForSelector('.sc-digi-button-h .digi-button--icon-secondary.sc-digi-button');
+
+                await page.click('.sc-digi-button-h .digi-button--icon-secondary.sc-digi-button');
+
+                await page.waitForSelector('.sc-digi-button-h .digi-button--icon-secondary.sc-digi-button');
+                await page.waitForSelector('.result-container');
+
+
+                return scrapeCurrentPage(); // Call this function recursively
+            }
+            await page.close();
+            console.log('scrapedData');
+            return scrapedData;
         }
+        let data = await scrapeCurrentPage();
+        console.log('data');
+        return data;
     }
 }
 
 startBrowser();
+
+// .digi-navigation-pagination__button--hidden.sc-digi-navigation-pagination
+// .digi-navigation-pagination__button.digi-navigation-pagination__button--previous.digi-navigation-pagination__button--hidden.sc-digi-navigation-pagination.sc-digi-button-h.sc-digi-button-s hydrated
+// .digi-navigation-pagination__button.digi-navigation-pagination__button--next.digi-navigation-pagination__button--hidden.sc-digi-navigation-pagination.sc-digi-button-h.sc-digi-button-s.hydrated
